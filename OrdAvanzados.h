4f50e0adb679ae0710d66ea74d4bdf278c6dfd63
HugoAdama <93099362+HugoAdama@users.noreply.github.com>
2022-06-24 02:37:17 -0500

    actualizado

--- OrdAvanzados.h
+++ OrdAvanzados.h
@@ -1,41 +1,40 @@
 #pragma once
-#include <iostream>
-#include <fstream>
 using namespace std;
 //funcion del pivote y intercambio
-int particion(int* A, int p, int r) {
-	int x = A[r];
-	int i = p - 1;
-	for (int j = p; j < r; j++) {
-		if (A[j] <= x) {
-			i++;
-			swap(A[i], A[j]);
-		}
-	}
-	swap(A[i + 1], A[r]);
-	return i + 1;
+int particion(vector<string>& musicFound, int p, int r) {
+    string x = musicFound[r];
+    int i = p - 1;
+    for (int j = p; j < r; j++) {
+        if (musicFound[j] <= x) {
+            i++;
+            swap(musicFound[i], musicFound[j]);
+        }
+    }
+    swap(musicFound[i + 1], musicFound[r]);
+    return i + 1;
 }
 //Quicksort
 //funcion donde se devuelve el indice del pivote
-void quicksort(int* A, int p, int r) {
-	int q;
-	if (p < r) {
-		q = particion(A, p, r);
-		quicksort(A, p, q - 1);
-		quicksort(A, q + 1, r);
-	}
+void quicksort(vector<string>& musicFound, int p, int r) {
+    int q;
+    if (p < r) {
+        q = particion(musicFound, p, r);
+        quicksort(musicFound, p, q - 1);
+        quicksort(musicFound, q + 1, r);
+    }
 }
-//Quickselect
-//para numero elementos kesimo
-int quickselect(int* A, int p, int r, int k) {
-	if (p == r) return A[p];
-	int q = particion(A, p, r);
-	int l = q - p + 1;
-	if (k == l)
-		return A[q];
-	else if (k < 1)
-		return quickselect(A, p, q - 1, k);
-	else
-		return quickselect(A, q + 1, r, k - l);
 
+//Quickselect}
+//para numero elementos kesimo
+int quickselect(vector<string>& musicFound, int p, int r, int k) {
+    //  if (p == r) 
+   // return musicFound[p];//indice del pivote
+    int q = particion(musicFound, p, r);
+    int l = q - p + 1;// sub lista para el kesimo elemento
+    if (k == l)
+     //return musicFound[q];
+     //else if (k < 1)
+        return quickselect(musicFound, p, q - 1, k);
+    else
+        return quickselect(musicFound, q + 1, r, k - l);
 }
