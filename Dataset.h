b2d5b51d4288501c48be9684488105e5c73495bf
Dámaris <67938957+Tasaycovil@users.noreply.github.com>
2022-06-28 14:01:42 -0500

    Update Dataset.h

--- Dataset.h
+++ Dataset.h
@@ -4,140 +4,63 @@
 #include <fstream>
 #include <string>
 #include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <windows.h>
 #include <cstdlib>
 #include <vector>
+#include <string>
 #include "Lista.h"
 #include "Agregar.h"
 #include "Reproduccion.h"
 using namespace std;
-void DatasetCancion(vector<string>& listMusic)
+void Datasetcancion()
 {
-    int i;
-    if (i == 1) {
-        for (unsigned int i = 0; i < 1; i++)
-        {
-            wstring temp = wstring(listMusic[i].begin(), listMusic[i].end());
-            LPCWSTR wideString = temp.c_str();
-            cout << "Pista actual: "; cout.put(14); cout << " " << listMusic[i] << endl;
-            PlaySound(wideString, NULL, SND_FILENAME | SND_ASYNC);
-            cout << "\t>>>Siguiente cancion, pulse cualquier tecla" << endl;
-            cout << "\t  ";
-            system("Pause");
-        }
-        if (listMusic.size() == 0)
-            cout << "\n\tNo hay canciones en la lista de reproduccion" << endl;
-        else
-            PlaySound(NULL, 0, 0);
-    }
-    else if (i == 2) {
-        for (unsigned int i = 0; i < 2; i++)
-        {
-            wstring temp = wstring(listMusic[i].begin(), listMusic[i].end());
-            LPCWSTR wideString = temp.c_str();
-            cout << "Pista actual: "; cout.put(14); cout << " " << listMusic[i] << endl;
-            PlaySound(wideString, NULL, SND_FILENAME | SND_ASYNC);
-            cout << "\t>>>Siguiente cancion, pulse cualquier tecla" << endl;
-            cout << "\t  ";
-            system("Pause");
-        }
-        if (listMusic.size() == 0)
-            cout << "\n\tNo hay canciones en la lista de reproduccion" << endl;
-        else
-            PlaySound(NULL, 0, 0);
-    }
-    /*int i;
-        fflush(stdin);
-        if (i == 1)
-            system("cd.. & dir *.wav /b /s > Debug\\Human.wav");
-            //cabeza = insertar_nodo(1, "Human", "cd.. & dir * .wav / b / s > Debug\\Human.wav", cabeza);
-        wstring temp = wstring(listMusic[i].begin(), listMusic[i].end());
-        LPCWSTR wideString = temp.c_str();
-        sndPlaySound("cd.. & dir *.wav /b /s > Debug\\Human.wav",0);
-        /*else if (i == 2) {
-            cabeza = insertar_nodo(1, "Human", "cd.. & dir * .wav / b / s > Debug\\Human.wav", cabeza);
-            cabeza = insertar_nodo(2, "All of me", "cd.. & dir * .wav / b / s > Debug\\All of me.wav", cabeza);
-            cout << "Pista actual: "; cout.put(14); cout << " " << listMusic[i] << endl;
-            PlaySound(wideString, NULL, SND_FILENAME | SND_ASYNC);
-        }
-        /*else if (i == 3) {
-            cabeza = insertar_nodo(1, "Christina Perri - Human", "D:\Dámaris\Song.wav\\Christina Perri - Human.wav", cabeza);
-            cabeza = insertar_nodo(2, "John Legend - All of me", "&#8234;D:\Dámaris\Song.wav\\John Legend - All of me.wav", cabeza);
-            cabeza = insertar_nodo(3, "Lewis Capaldi - Someone you loved", "&#8234;D:\Dámaris\Song.wav\\Lewis Capaldi - Someone you loved.wav", cabeza);
-        }
-        else if (i == 4) {
-            cabeza = insertar_nodo(1, "Christina Perri - Human", "D:\Dámaris\Song.wav\\Christina Perri - Human.wav", cabeza);
-            cabeza = insertar_nodo(2, "John Legend - All of me", "&#8234;D:\Dámaris\Song.wav\\John Legend - All of me.wav", cabeza);
-            cabeza = insertar_nodo(3, "Lewis Capaldi - Someone you loved", "&#8234;D:\Dámaris\Song.wav\\Lewis Capaldi - Someone you loved.wav", cabeza);
-            cabeza = insertar_nodo(4, "Jessica Mauboy - Little Things", "&#8234;D:\Dámaris\Song.wav\\Jessica Mauboy - Little Things.wav", cabeza);
-        }
-        else if (i == 5) {
-            cabeza = insertar_nodo(1, "Christina Perri - Human", "D:\Dámaris\Song.wav\\Christina Perri - Human.wav", cabeza);
-            cabeza = insertar_nodo(2, "John Legend - All of me", "&#8234;D:\Dámaris\Song.wav\\John Legend - All of me.wav", cabeza);
-            cabeza = insertar_nodo(3, "Lewis Capaldi - Someone you loved", "&#8234;D:\Dámaris\Song.wav\\Lewis Capaldi - Someone you loved.wav", cabeza);
-            cabeza = insertar_nodo(4, "Jessica Mauboy - Little Things", "&#8234;D:\Dámaris\Song.wav\\Jessica Mauboy - Little Things.wav", cabeza);
-            cabeza = insertar_nodo(5, "Jessica Mauboy - Never Be the Same", "&#8234;D:\Dámaris\Song.wav\\Jessica Mauboy - Never Be the Same.wav", cabeza);
-        }
-        else if (i == 6) {
-            cabeza = insertar_nodo(1, "Christina Perri - Human", "D:\Dámaris\Song.wav\\Christina Perri - Human.wav", cabeza);
-            cabeza = insertar_nodo(2, "John Legend - All of me", "&#8234;D:\Dámaris\Song.wav\\John Legend - All of me.wav", cabeza);
-            cabeza = insertar_nodo(3, "Lewis Capaldi - Someone you loved", "&#8234;D:\Dámaris\Song.wav\\Lewis Capaldi - Someone you loved.wav", cabeza);
-            cabeza = insertar_nodo(4, "Jessica Mauboy - Little Things", "&#8234;D:\Dámaris\Song.wav\\Jessica Mauboy - Little Things.wav", cabeza);
-            cabeza = insertar_nodo(5, "Jessica Mauboy - Never Be the Same", "&#8234;D:\Dámaris\Song.wav\\Jessica Mauboy - Never Be the Same.wav", cabeza);
-            cabeza = insertar_nodo(6, "The Cab - Angel with a Shotgun", "&#8234;D:\Dámaris\Song.wav\\The Cab - Angel with a Shotgun.wav", cabeza);
-        }
-        else if (i == 7) {
-            cabeza = insertar_nodo(1, "Christina Perri - Human", "D:\Dámaris\Song.wav\\Christina Perri - Human.wav", cabeza);
-            cabeza = insertar_nodo(2, "John Legend - All of me", "&#8234;D:\Dámaris\Song.wav\\John Legend - All of me.wav", cabeza);
-            cabeza = insertar_nodo(3, "Lewis Capaldi - Someone you loved", "&#8234;D:\Dámaris\Song.wav\\Lewis Capaldi - Someone you loved.wav", cabeza);
-            cabeza = insertar_nodo(4, "Jessica Mauboy - Little Things", "&#8234;D:\Dámaris\Song.wav\\Jessica Mauboy - Little Things.wav", cabeza);
-            cabeza = insertar_nodo(5, "Jessica Mauboy - Never Be the Same", "&#8234;D:\Dámaris\Song.wav\\Jessica Mauboy - Never Be the Same.wav", cabeza);
-            cabeza = insertar_nodo(6, "The Cab - Angel with a Shotgun", "&#8234;D:\Dámaris\Song.wav\\The Cab - Angel with a Shotgun.wav", cabeza);
-            cabeza = insertar_nodo(7, "musica5", "&#8234;D:\Dámaris\Song.wav\\musica5.wav", cabeza);
-        }
-        else if (i == 8) {
-            cabeza = insertar_nodo(1, "Christina Perri - Human", "D:\Dámaris\Song.wav\\Christina Perri - Human.wav", cabeza);
-            cabeza = insertar_nodo(2, "John Legend - All of me", "&#8234;D:\Dámaris\Song.wav\\John Legend - All of me.wav", cabeza);
-            cabeza = insertar_nodo(3, "Lewis Capaldi - Someone you loved", "&#8234;D:\Dámaris\Song.wav\\Lewis Capaldi - Someone you loved.wav", cabeza);
-            cabeza = insertar_nodo(4, "Jessica Mauboy - Little Things", "&#8234;D:\Dámaris\Song.wav\\Jessica Mauboy - Little Things.wav", cabeza);
-            cabeza = insertar_nodo(5, "Jessica Mauboy - Never Be the Same", "&#8234;D:\Dámaris\Song.wav\\Jessica Mauboy - Never Be the Same.wav", cabeza);
-            cabeza = insertar_nodo(6, "The Cab - Angel with a Shotgun", "&#8234;D:\Dámaris\Song.wav\\The Cab - Angel with a Shotgun.wav", cabeza);
-            cabeza = insertar_nodo(7, "musica5", "&#8234;D:\Dámaris\Song.wav\\musica5.wav", cabeza);
-            cabeza = insertar_nodo(8, "James Bay - Move together", "&#8234;D:\Dámaris\Song.wav\\James Bay - Move together.wav", cabeza);
-        }
-        else if (i == 9) {
-            cabeza = insertar_nodo(1, "Christina Perri - Human", "D:\Dámaris\Song.wav\\Christina Perri - Human.wav", cabeza);
-            cabeza = insertar_nodo(2, "John Legend - All of me", "&#8234;D:\Dámaris\Song.wav\\John Legend - All of me.wav", cabeza);
-            cabeza = insertar_nodo(3, "Lewis Capaldi - Someone you loved", "&#8234;D:\Dámaris\Song.wav\\Lewis Capaldi - Someone you loved.wav", cabeza);
-            cabeza = insertar_nodo(4, "Jessica Mauboy - Little Things", "&#8234;D:\Dámaris\Song.wav\\Jessica Mauboy - Little Things.wav", cabeza);
-            cabeza = insertar_nodo(5, "Jessica Mauboy - Never Be the Same", "&#8234;D:\Dámaris\Song.wav\\Jessica Mauboy - Never Be the Same.wav", cabeza);
-            cabeza = insertar_nodo(6, "The Cab - Angel with a Shotgun", "&#8234;D:\Dámaris\Song.wav\\The Cab - Angel with a Shotgun.wav", cabeza);
-            cabeza = insertar_nodo(7, "musica5", "&#8234;D:\Dámaris\Song.wav\\musica5.wav", cabeza);
-            cabeza = insertar_nodo(8, "James Bay - Move together", "&#8234;D:\Dámaris\Song.wav\\James Bay - Move together.wav", cabeza);
-            cabeza = insertar_nodo(9, "Chord Overstreet - Hold On", "&#8234;D:\Dámaris\Song.wav\\Chord Overstreet - Hold On.wav", cabeza);
-        }
-        else if (i == 10) {
-            cabeza = insertar_nodo(1, "Christina Perri - Human", "D:\Dámaris\Song.wav\\Christina Perri - Human.wav", cabeza);
-            cabeza = insertar_nodo(2, "John Legend - All of me", "&#8234;D:\Dámaris\Song.wav\\John Legend - All of me.wav", cabeza);
-            cabeza = insertar_nodo(3, "Lewis Capaldi - Someone you loved", "&#8234;D:\Dámaris\Song.wav\\Lewis Capaldi - Someone you loved.wav", cabeza);
-            cabeza = insertar_nodo(4, "Jessica Mauboy - Little Things", "&#8234;D:\Dámaris\Song.wav\\Jessica Mauboy - Little Things.wav", cabeza);
-            cabeza = insertar_nodo(5, "Jessica Mauboy - Never Be the Same", "&#8234;D:\Dámaris\Song.wav\\Jessica Mauboy - Never Be the Same.wav", cabeza);
-            cabeza = insertar_nodo(6, "The Cab - Angel with a Shotgun", "&#8234;D:\Dámaris\Song.wav\\The Cab - Angel with a Shotgun.wav", cabeza);
-            cabeza = insertar_nodo(7, "musica5", "&#8234;D:\Dámaris\Song.wav\\musica5.wav", cabeza);
-            cabeza = insertar_nodo(8, "James Bay - Move together", "&#8234;D:\Dámaris\Song.wav\\James Bay - Move together.wav", cabeza);
-            cabeza = insertar_nodo(9, "Chord Overstreet - Hold On", "&#8234;D:\Dámaris\Song.wav\\Chord Overstreet - Hold On.wav", cabeza);
-            cabeza = insertar_nodo(10, "Jennie-Can't Take My Eyes Off You", "&#8234;D:\Dámaris\Song.wav\\Jennie-Can't Take My Eyes Off You.wav", cabeza);
-        }
-        wstring temp = wstring(listMusic[i].begin(), listMusic[i].end());
-            LPCWSTR wideString = temp.c_str();
-            cout << "Pista actual: "; cout.put(14); cout << " " << listMusic[i] << endl;
-            PlaySound(wideString, NULL, SND_FILENAME | SND_ASYNC);
-        cout << "\t>>>Siguiente cancion, pulse cualquier tecla" << endl;
+    int n;
+    if (n == 1) {
+        cout << "Musica actual: "; cout.put(14); cout << " " << "Human" << endl;
+        cout << "\t>>>Si desea salir, pulse cualquier tecla" << endl;
         cout << "\t  ";
+        PlaySound(TEXT("Human1.wav"), NULL, SND_SYNC);
         system("Pause");
     }
-    if (listMusic.size() == 0)
-        cout << "\n\tNo hay canciones en la lista de reproduccion" << endl;
-    else
-        PlaySound(NULL, 0, 0);*/
+    else if (n == 2) {
+        cout << "Musica actual: "; cout.put(14); cout << " " << "All of me" << endl;
+        cout << "\t>>>Si desea cambiar la cancion, por favor, pulse cualquier tecla" << endl;
+        PlaySound(TEXT("All of me1.wav"), NULL, SND_SYNC);
+        cout << "Musica actual: "; cout.put(14); cout << " " << "Move together" << endl;
+        cout << "\t>>>Si desea salir, por favor, por favor, pulse cualquier tecla" << endl;
+        cout << "\t  ";
+        PlaySound(TEXT("Move together1.wav"), NULL, SND_SYNC);
+        system("Pause");
+    }
+    else if (n == 3) {
+        cout << "Musica actual: "; cout.put(14); cout << " " << "Hold On" << endl;
+        cout << "\t>>>Si desea cambiar la cancion, por favor, pulse cualquier tecla" << endl;
+        PlaySound(TEXT("Hold On1.wav"), NULL, SND_SYNC);
+        cout << "Musica actual: "; cout.put(14); cout << " " << "Angel with a Shotgun" << endl;
+        cout << "\t>>>Si desea cambiar la cancion, por favor, pulse cualquier tecla" << endl;
+        PlaySound(TEXT("Angel with a Shotgun1.wav"), NULL, SND_SYNC);
+        cout << "Musica actual: "; cout.put(14); cout << " " << "musica5" << endl;
+        cout << "\t>>>Si desea salir, por favor, pulse cualquier tecla" << endl;
+        cout << "\t  ";
+        PlaySound(TEXT("musica5_1.wav"), NULL, SND_SYNC);
+        system("Pause");
+    }
+    /*else if (n == 4) {
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "Move together" << endl;
+                    cout << "\t>>>Si desea cambiar la cancion, por favor, pulse cualquier tecla" << endl;
+                    PlaySound(TEXT("Move together1.wav"), NULL, SND_SYNC);
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "Human" << endl;
+                    cout << "\t>>>Si desea cambiar la cancion, por favor, pulse cualquier tecla" << endl;
+                    PlaySound(TEXT("Human1.wav"), NULL, SND_SYNC);
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "Hold On" << endl;
+                    cout << "\t>>>Si desea cambiar la cancion, por favor, pulse cualquier tecla" << endl;
+                    PlaySound(TEXT("Hold On1.wav"), NULL, SND_SYNC);
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "musica5" << endl;
+                    cout << "\t>>>Si desea salir, por favor, pulse cualquier tecla" << endl;
+                    cout << "\t  ";
+                    PlaySound(TEXT("musica5_1.wav"), NULL, SND_SYNC);
+                    system("Pause");
+                }*/
 }
