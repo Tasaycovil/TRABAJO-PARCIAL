ad4a9d6201f57bf34474bb41901e5c3536797ae8
Dámaris <67938957+Tasaycovil@users.noreply.github.com>
2022-06-28 14:02:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
-#include "FuncionesMain.h"
+#include <windows.h>
+#include <mmsystem.h>
+#include "Lista.h"
+#include "Agregar.h"
+#include "Eliminar.h"
+#include "Reproduccion.h"
 #include "OrdLista.h"
 #include "OrdAgregar.h"
-#include "OrdReproducir.h"
 #include "OrdAvanzados.h" 
-#include "Dataset.h"
-#include "HashEntidad.h"
 #include "HashTabla.h"
-
+#include "HashEntidad.h"
+#include "Dataset.h"
 using namespace std;
 using namespace System;
-
 
 int main()
 {
     int opc;
-	bool repetir = true;
+    bool repetir = true;
     vector<string> listMusic;
     vector<string> musicFound;
     //metodo quicksort 
@@ -23,40 +25,38 @@ int main()
     int n = 5;
     //caso de valor kesimo 2 para quickselect
     int k = 2;
-
-    while(true)
+    while (true)
     {
-      do
-		  {
-          cout << "**********      BIENVENIDO A PLAYMUSIC  *******************" << endl;
-          cout << "**********       MENU DE OPCIONES       *******************" << endl;
-	      cout << "*    1. LISTA DE CANCIONES ENCONTRADAS                    *" << endl;
-		  cout << "*    2. AGREGAR CANCION PARA REPRODUCIR                   *" << endl;
-		  cout << "*    3. ELIMINAR CANCION DE LA LISTA DE REPRODUCCION      *" << endl;
-		  cout << "*    4. REPRODUCIR CANCION                                *" << endl;
-          cout << "*    5. IMPRESION DE LA LISTA DE CANCIONES POR QUICKSORT  *" << endl;
-          cout << "*    6. LISTA DE ELECCION DE CANCIONES ALEATORIAS         *" << endl;
-          cout << "*    7. INSERCION POR HASHTABLE                           *" << endl;
-          cout << "*    8. Salir                                             *" << endl;
-          cout << "***********************************************************" << endl;
-	      cout << "INGRESE SU ELECCION : ";
-			  cin >> opc;
-		  } 
-      while (opc < 1 || opc>7);
-      if(opc==8) break;
+        do
+        {
+            cout << "****      BIENVENIDO A PLAYMUSIC  *******" << endl;
+            cout << "****       MENU DE OPCIONES       *******" << endl;
+            cout << "*    1. LISTA DE CANCIONES                  *" << endl;
+            cout << "*    2. AGREGAR CANCION PARA REPRODUCIR                   *" << endl;
+            cout << "*    3. ELIMINAR CANCION DE LA LISTA DE REPRODUCCION      *" << endl;
+            cout << "*    4. REPRODUCIR CANCION                                *" << endl;
+            cout << "*    5. IMPRESION DE LA LISTA DE CANCIONES POR QUICKSORT  *" << endl;
+            cout << "*    6. RANGO DE CANCIONES  *" << endl;
+            cout << "*    7. INSERCION POR HASHTABLE        *" << endl;
+            cout << "*    8. Salir                                             *" << endl;
+            cout << "*********************" << endl;
+            cout << "INGRESE SU ELECCION : ";
+            cin >> opc;
+        }       while (opc < 1 || opc>8);
+        if (opc == 8) break;
 
-      switch(opc)
-      {
+        switch (opc)
+        {
         case 1:
             cout << "\nCanciones encontradas:\n";
             listarCanciones(musicFound);
-          break;
-        case 2:agregarCancion(listMusic,musicFound);
-          break;
+            break;
+        case 2:agregarCancion(listMusic, musicFound);
+            break;
         case 3:eliminarCancion(listMusic);
-          break;
+            break;
         case 4:reproducirCancion(listMusic);
-          break;
+            break;
         case 5:
             //quicksort
             quicksort(musicFound, 0, n - 1);
@@ -72,18 +72,73 @@ int main()
             cout << "\nArreglo de canciones ordenado por quickselect cuando el kesimo elemento es 2 :\n" << e;
             break;*/
         case 6:
-            cout << "Escoge el numero de canciones aleatorias" << endl;
+            cout << "Numero de canciones aleatorias" << endl;
             cin >> n;
-            fflush(stdin);
-            DatasetCancion(listMusic);
+            /*PlaySound(TEXT("All of me.wav"), NULL,SND_SYNC);
+            cout << "\t>>>Siguiente cancion, pulse cualquier tecla" << endl;
+            cout << "\t  ";
+            system("Pause");
+            /*char soundfile[] = "D:\Dámaris\Song.wav\\All of me.wav";
+            cout << PlaySound((LPCWSTR)soundfile, NULL, SND_FILENAME | SND_ASYNC);
+            system("pause");
+            /*fflush(stdin);*/
+            void Datasetcancion(); {
+                if (n == 1) {
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "Human" << endl;
+                    cout << "\t>>>Si desea salir, pulse cualquier tecla" << endl;
+                    cout << "\t  ";
+                    PlaySound(TEXT("Human1.wav"), NULL, SND_SYNC);
+                    system("Pause");
+                }
+                else if (n == 2) {
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "All of me" << endl;
+                    cout << "\t>>>Si desea cambiar la cancion, por favor, pulse cualquier tecla" << endl;
+                    PlaySound(TEXT("All of me1.wav"), NULL, SND_SYNC);
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "Move together" << endl;
+                    cout << "\t>>>Si desea salir, por favor, por favor, pulse cualquier tecla" << endl;
+                    cout << "\t  ";
+                    PlaySound(TEXT("Move together1.wav"), NULL, SND_SYNC);
+                    system("Pause");
+                }
+                else if (n == 3) {
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "Hold On" << endl;
+                    cout << "\t>>>Si desea cambiar la cancion, por favor, pulse cualquier tecla" << endl;
+                    PlaySound(TEXT("Hold On1.wav"), NULL, SND_SYNC);
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "Angel with a Shotgun" << endl;
+                    cout << "\t>>>Si desea cambiar la cancion, por favor, pulse cualquier tecla" << endl;
+                    PlaySound(TEXT("Angel with a Shotgun1.wav"), NULL, SND_SYNC);
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "musica5" << endl;
+                    cout << "\t>>>Si desea salir, por favor, pulse cualquier tecla" << endl;
+                    cout << "\t  ";
+                    PlaySound(TEXT("musica5_1.wav"), NULL, SND_SYNC);
+                    system("Pause");
+                }
+                /*else if (n == 4) {
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "Move together" << endl;
+                    cout << "\t>>>Si desea cambiar la cancion, por favor, pulse cualquier tecla" << endl;
+                    PlaySound(TEXT("Move together1.wav"), NULL, SND_SYNC);
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "Human" << endl;
+                    cout << "\t>>>Si desea cambiar la cancion, por favor, pulse cualquier tecla" << endl;
+                    PlaySound(TEXT("Human1.wav"), NULL, SND_SYNC);
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "Hold On" << endl;
+                    cout << "\t>>>Si desea cambiar la cancion, por favor, pulse cualquier tecla" << endl;
+                    PlaySound(TEXT("Hold On1.wav"), NULL, SND_SYNC);
+                    cout << "Musica actual: "; cout.put(14); cout << " " << "musica5" << endl;
+                    cout << "\t>>>Si desea salir, por favor, pulse cualquier tecla" << endl;
+                    cout << "\t  ";
+                    PlaySound(TEXT("musica5_1.wav"), NULL, SND_SYNC);
+                    system("Pause");
+                }*/
+            }
             break;
-          break;
         case 7:
             RetornoTabla();
             break;
       }
     }
-    cout<<"FIN DEL PROGRAMA PLAYMUSIC"<<endl;
+    cout << "FIN DEL PROGRAMA PLAYMUSIC" << endl;
     system("pause");
-    cin.get();
+
+    _getch();
+
 	}
